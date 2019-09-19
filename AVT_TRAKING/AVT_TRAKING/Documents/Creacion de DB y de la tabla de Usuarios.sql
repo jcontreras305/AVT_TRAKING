6599fca30b63d62a36b405450ef72ee7615b7a72
Isaac <ilumfinity2114@gmail.com>
2019-09-18 20:09:54 -0500

    chargue Employees Image

--- AVT_TRAKING/AVT_TRAKING/Documents/Creacion de DB y de la tabla de Usuarios.sql
+++ AVT_TRAKING/AVT_TRAKING/Documents/Creacion de DB y de la tabla de Usuarios.sql
@@ -11,3 +11,60 @@ create table users(
 )
 
 insert into users values (NEWID(), 'admin' , 'admin')
+
+--############################################################################
+--############# Tables Employees , Contact , HomeAdress , PayRate ############
+--############################################################################
+use VRT_TRAKING
+
+create table employees(
+	idEmployee varchar(36) primary key not null,
+	numberEmploye int not null,
+	firstName varchar(30),
+	lastName varchar(25),
+	middleName varchar(25),
+	socialNumber varchar(14),
+	SAPNumber int,
+	photo image,
+	idHomeAdress varchar(36),
+	idContact varchar(36),
+	idPayRate varchar(36),
+	estatus char(1)
+)
+
+alter table employees 
+add constraint fk_idPayRate_employees
+foreign key (idPayRate) references payRate(idPayRate)
+
+alter table employees 
+add constraint fk_idContact_employees
+foreign key (idContact) references contact(idContact)
+
+alter table employees 
+add constraint fk_idHomeAdress_employees
+foreign key (idHomeAdress) references HomeAddress(idHomeAdress)
+
+
+create table contact(
+	idContact varchar(36) primary key not null,
+	phoneNumber1 varchar(13),
+	phoneNumber2 varchar(13),
+	emial varchar(50)
+)
+
+create table HomeAddress (
+	idHomeAdress varchar(36) primary key not null,
+	avenue varchar(80),
+	number int,
+	city varchar(20), 
+	providence varchar(20),
+	postalCode int
+)
+
+create table payRate(
+	idPayRate varchar(36) primary key not null ,
+	payRate1 float,
+	payRate2 float, 
+	payRate3 float
+)
+
---
+++ AVT_TRAKING/AVT_TRAKING/Froms/Employees.vb
@@ -0,0 +1,13 @@
+﻿Public Class Employees
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnChooseImage.Click
+        Try
+            Dim file As New OpenFileDialog
+            file.Filter = "Imagenes JPG|*.jpg|Images PNG|*.png"
+            If file.ShowDialog = Windows.Forms.DialogResult.OK Then
+                imgPhoto.Image = Image.FromFile(file.FileName)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
