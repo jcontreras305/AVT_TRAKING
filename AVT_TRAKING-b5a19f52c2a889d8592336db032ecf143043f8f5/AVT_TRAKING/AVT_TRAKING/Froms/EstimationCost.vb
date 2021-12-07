04c764e309d5f7f390f5bed7112292689cb5ee4c
Rose-19971206 <68295203+Rose-19971206@users.noreply.github.com>
2021-12-07 15:08:17 -0600

    Diseño interfaz Estimacion de costos, ajustes a reportes

--- AVT_TRAKING-b5a19f52c2a889d8592336db032ecf143043f8f5/AVT_TRAKING/AVT_TRAKING/Froms/EstimationCost.vb
+++ AVT_TRAKING-b5a19f52c2a889d8592336db032ecf143043f8f5/AVT_TRAKING/AVT_TRAKING/Froms/EstimationCost.vb
@@ -1,4 +1,5 @@
-﻿Class EstimationCost
+﻿Imports System.Runtime.InteropServices
+Class EstimationCost
     Dim mtdEstiation As New EstimationSC
     Dim selectTable As String = ""
     Private Sub EstimationCost_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -100,5 +101,34 @@
             tblFactorSC.Rows(e.RowIndex).Cells(0).Value = If(tblFactorSC.Rows(e.RowIndex).Cells(0).Value Is DBNull.Value, "0", tblFactorSC.Rows(e.RowIndex).Cells(0).Value)
             tblFactorSC.Rows(e.RowIndex).Cells(2).Value = If(tblFactorSC.Rows(e.RowIndex).Cells(2).Value Is DBNull.Value, "0", tblFactorSC.Rows(e.RowIndex).Cells(2).Value)
         End If
+    End Sub
+
+    Private Sub btnMaximize_Click(sender As Object, e As EventArgs) Handles btnMaximize.Click
+        WindowState = FormWindowState.Maximized
+        btnMaximize.Visible = False
+        btnRestore.Visible = True
+    End Sub
+
+    Private Sub btnRestore_Click(sender As Object, e As EventArgs) Handles btnRestore.Click
+        WindowState = FormWindowState.Normal
+        btnRestore.Visible = False
+        btnMaximize.Visible = True
+    End Sub
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(hWnd As IntPtr, wMsg As Integer, wParam As Integer, lParam As Integer)
+    End Sub
+
+
+    Private Sub TitleBar_MouseMove(sender As Object, e As MouseEventArgs) Handles TitleBar.MouseMove
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        Me.WindowState = FormWindowState.Minimized
     End Sub
 End Class
--- AVT_TRAKING-b5a19f52c2a889d8592336db032ecf143043f8f5/AVT_TRAKING/AVT_TRAKING/My Project/Resources.Designer.vb
+++ AVT_TRAKING-b5a19f52c2a889d8592336db032ecf143043f8f5/AVT_TRAKING/AVT_TRAKING/My Project/Resources.Designer.vb
@@ -223,6 +223,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property estimation() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("estimation", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property exit2() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("exit2", resourceCulture)
